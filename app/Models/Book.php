2e064eeed8fbb9531748332fd8cc5b5ef0cab6e9
MohamedJamh <jam7.mohamed@gmail.com>
2023-03-14 21:53:12 +0100

    migration v1

---
+++ app/Models/Book.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "isbn",
+        "title",
+        "date_publication",
+        "number_pages",
+        "auteur_id",
+        "collection_id",
+        "genre_id"
+    ];
+}
---
+++ database/migrations/2023_03_14_302739_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->unsignedBigInteger("isbn")->primary();
+            $table->string("title");
+            $table->date("date_publication");
+            $table->integer("number_pages");
+            $table->foreignId("auteur_id")->constrained();
+            $table->foreignId("genre_id")->constrained();
+            $table->foreignId("collection_id")->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
